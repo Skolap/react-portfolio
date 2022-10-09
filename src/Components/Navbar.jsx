688c2749e1e83387580116591c124f5c79fa2de5
Shailesh Kolap <skolap22@gmail.com>
2022-10-09 23:50:55 +0530

    Added social icons

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 import logo from "../Assets/logo.png";
 
 const Navbar = () => {
@@ -13,7 +15,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={logo} alt="Logo Image" style={{ width: "80px" }} />
+        <img src={logo} alt="logo" style={{ width: "80px" }} />
       </div>
 
       {/* Menu */}
@@ -51,7 +53,46 @@ const Navbar = () => {
       </div>
 
       {/* Social Icons */}
-      <div className="hidden"></div>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          {/* LinkedIn */}
+          <li className="w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/skolap/"
+            >
+              LinkedIn <FaLinkedinIn size={30} />{" "}
+            </a>
+          </li>
+          {/* Github */}
+          <li className="w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/Skolap"
+            >
+              Github <FaGithub size={30} />{" "}
+            </a>
+          </li>
+          {/* Email */}
+          <li className="w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10px] duration-300 bg-[#CC3C2F]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="mailto:skolap22@gmail.com"
+            >
+              Email <HiOutlineMail size={30} />{" "}
+            </a>
+          </li>
+          {/* Resume */}
+          <li className="w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="ab.com"
+            >
+              Resume <BsFillPersonLinesFill size={30} />{" "}
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
