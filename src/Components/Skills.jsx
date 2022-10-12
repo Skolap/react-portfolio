91b96169608944eb828e2d80a0409b0fcfada30f
Shailesh Kolap <skolap22@gmail.com>
2022-10-12 09:34:44 +0530

    Import icons from Assets

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,4 +1,16 @@
 import React from "react";
+import Css from "../Assets/css.png";
+import Express from '../Assets/Express.png'
+import Firebase from "../Assets/firebase.png";
+import Git from "../Assets/git.png";
+import Html from "../Assets/html.png";
+import Javascript from "../Assets/javascript.png";
+import Jest from "../Assets/jest.png";
+import Jira from "../Assets/jira.png";
+import Mongo from "../Assets/mongo.png";
+import Node from "../Assets/node.png";
+import ReactLogo from "../Assets/react.png";
+import Tailwind from "../Assets/tailwind.png";
 
 const Skills = () => {
   return <div>Skills</div>;
