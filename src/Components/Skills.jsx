396db4b80d19e4f53a5860a0daafdca51f70fb2f
Shailesh Kolap <skolap22@gmail.com>
2022-10-12 19:26:15 +0530

    Complated Skills Section

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Css from "../Assets/css.png";
-import Express from '../Assets/Express.png'
+import Express from "../Assets/Express.png";
 import Firebase from "../Assets/firebase.png";
 import Git from "../Assets/git.png";
 import Html from "../Assets/html.png";
@@ -13,7 +13,81 @@ import ReactLogo from "../Assets/react.png";
 import Tailwind from "../Assets/tailwind.png";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div name="skills" className="bg-[#0a192f] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4">Experience</p>
+          <p className="py-4">// These are technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-5 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
+            <img className="w-20 mx-auto" src={Html} alt="HTML Icon" />
+            <p>HTML</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
+            <img className="w-20 mx-auto" src={Css} alt="CSS Icon" />
+            <p>CSS</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
+            <img
+              className="w-20 mx-auto"
+              src={Javascript}
+              alt="JavaScript Icon"
+            />
+            <p>JAVASCRIPT</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
+            <img className="w-20 mx-auto" src={ReactLogo} alt="React Icon" />
+            <p>REACT</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
+            <img
+              className="w-21 h-20 mx-auto"
+              src={Express}
+              alt="Express Icons2"
+            />
+            <p>EXPRESS</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
+            <img className="w-20 mx-auto" src={Git} alt="Git Icon" />
+            <p>GIT</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
+            <img className="w-20 mx-auto" src={Node} alt="Node Icon" />
+            <p>NODE</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
+            <img className="w-20 mx-auto" src={Mongo} alt="Mongo Icon" />
+            <p>MONGODB</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
+            <img
+              className="w-20 mx-auto"
+              src={Tailwind}
+              alt="TailwindCss Icon"
+            />
+            <p>TAILWIND CSS</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
+            <img className="w-20 mx-auto" src={Jest} alt="Jest Icon" />
+            <p>JEST</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
