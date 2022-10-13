bccc76ef02fefcb902fb46c2abf698fafcd0fefe
Shailesh Kolap <skolap22@gmail.com>
2022-10-13 09:28:20 +0530

    Commented unused import

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Css from "../Assets/css.png";
 import Express from "../Assets/Express.png";
-import Firebase from "../Assets/firebase.png";
+// import Firebase from "../Assets/firebase.png";
 import Git from "../Assets/git.png";
 import Html from "../Assets/html.png";
 import Javascript from "../Assets/javascript.png";
 import Jest from "../Assets/jest.png";
-import Jira from "../Assets/jira.png";
+// import Jira from "../Assets/jira.png";
 import Mongo from "../Assets/mongo.png";
 import Node from "../Assets/node.png";
 import ReactLogo from "../Assets/react.png";
