911803d3df6cf161fde23232a79057cdc6b820bd
Shailesh Kolap <skolap22@gmail.com>
2022-10-13 10:05:25 +0530

    Added comments in skills section

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -19,20 +19,23 @@ const Skills = () => {
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4">Experience</p>
-          <p className="py-4">// These are technologies I've worked with</p>
+          <p className="py-4"> These are technologies I've worked with</p>
         </div>
 
+        {/* HTML */}
         <div className="w-full grid grid-cols-2 sm:grid-cols-5 gap-4 text-center py-8">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
             <img className="w-20 mx-auto" src={Html} alt="HTML Icon" />
             <p>HTML</p>
           </div>
 
+          {/* CSS */}
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
             <img className="w-20 mx-auto" src={Css} alt="CSS Icon" />
             <p>CSS</p>
           </div>
 
+          {/* JavaScript */}
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
             <img
               className="w-20 mx-auto"
@@ -42,11 +45,13 @@ const Skills = () => {
             <p>JAVASCRIPT</p>
           </div>
 
+          {/* React */}
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
             <img className="w-20 mx-auto" src={ReactLogo} alt="React Icon" />
             <p>REACT</p>
           </div>
 
+          {/* Express */}
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
             <img
               className="w-21 h-20 mx-auto"
@@ -56,21 +61,25 @@ const Skills = () => {
             <p>EXPRESS</p>
           </div>
 
+          {/* Git */}
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
             <img className="w-20 mx-auto" src={Git} alt="Git Icon" />
             <p>GIT</p>
           </div>
 
+          {/* Node js */}
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
             <img className="w-20 mx-auto" src={Node} alt="Node Icon" />
             <p>NODE</p>
           </div>
 
+          {/* Mongo db */}
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
             <img className="w-20 mx-auto" src={Mongo} alt="Mongo Icon" />
             <p>MONGODB</p>
           </div>
 
+          {/* Tailwind CSS */}
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
             <img
               className="w-20 mx-auto"
@@ -80,6 +89,7 @@ const Skills = () => {
             <p>TAILWIND CSS</p>
           </div>
 
+          {/* Jest */}
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-700">
             <img className="w-20 mx-auto" src={Jest} alt="Jest Icon" />
             <p>JEST</p>
