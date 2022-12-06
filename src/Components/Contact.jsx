e3d9bd35041d7f8f309aeedaf77d27803664be1a
Shailesh Kolap <skolap22@gmail.com>
2022-12-06 19:16:08 +0530

    correct typo of email in contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
           </p>
           <p className="text-gray-300 py-4">
             Submit the form below or mail me at{" "}
-            <a href="mailto:skolap22@gmail.com">
+            <a href="mailto:contact@shailesh.org">
               <strong>contact@shailesh.org</strong>
             </a>
           </p>
